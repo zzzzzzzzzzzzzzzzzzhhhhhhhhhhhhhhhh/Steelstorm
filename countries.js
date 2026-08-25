635fc8def182e608ee408858efd13f339f1167b9
Zhr <yep_zi_hao@students.edu.sg>
2026-08-25 10:31:08 +0800

    Add real and fictional country roster

---
+++ countries.js
@@ -0,0 +1,8 @@
+const REAL_COUNTRIES=[
+['Singapore','SGP','Asia'],['Malaysia','MYS','Asia'],['Indonesia','IDN','Asia'],['Thailand','THA','Asia'],['Vietnam','VNM','Asia'],['Philippines','PHL','Asia'],['China','CHN','Asia'],['Japan','JPN','Asia'],['South Korea','KOR','Asia'],['India','IND','Asia'],['Australia','AUS','Oceania'],['New Zealand','NZL','Oceania'],['United States','USA','Americas'],['Canada','CAN','Americas'],['Mexico','MEX','Americas'],['Brazil','BRA','Americas'],['Argentina','ARG','Americas'],['United Kingdom','GBR','Europe'],['France','FRA','Europe'],['Germany','DEU','Europe'],['Spain','ESP','Europe'],['Italy','ITA','Europe'],['Poland','POL','Europe'],['Ukraine','UKR','Europe'],['Russia','RUS','Europe/Asia'],['Türkiye','TUR','Europe/Asia'],['Egypt','EGY','Africa'],['South Africa','ZAF','Africa'],['Nigeria','NGA','Africa'],['Kenya','KEN','Africa'],['Saudi Arabia','SAU','Asia'],['United Arab Emirates','ARE','Asia'],['Israel','ISR','Asia'],['Iran','IRN','Asia'],['Pakistan','PAK','Asia'],['Bangladesh','BGD','Asia']
+];
+const FICTIONAL_COUNTRIES=[
+['Arcton','ARC','Fictional'],['Northstar','NTS','Fictional'],['Iron Coast','IRC','Fictional'],['Sable Union','SBU','Fictional'],['Central States','CST','Fictional'],['Eastern Front','ETF','Fictional'],['Southern Front','SOF','Fictional']
+];
+const WORLD_COUNTRIES=[...REAL_COUNTRIES,...FICTIONAL_COUNTRIES];
+window.STEELSTORM_COUNTRIES={REAL_COUNTRIES,FICTIONAL_COUNTRIES,WORLD_COUNTRIES};
